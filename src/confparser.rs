d517a6f87286476d8ae5f29ad6bb2ac4fb07e4dd
Euan Kemp <euank@euank.com>
2020-07-13 23:04:48 -0700

    confparser: add tests

--- src/confparser.rs
+++ src/confparser.rs
@@ -1,6 +1,7 @@
 use nom::IResult;
 use std::default::Default;
 use ipnet::{IpNet, Ipv4Net};
+use std::net::IpAddr;
 use nom::bytes::complete::tag;
 use nom::character::complete::{digit1, space1};
 use nom::character::is_space;
@@ -12,11 +13,13 @@ use nom::combinator::{map, map_res, not};
 use nom::sequence::preceded;
 
 // IpSet is the argument that allow and deny take.
+#[derive(Debug, PartialEq, Eq)]
 enum IpSet {
     All,
     Cidr(IpNet),
 }
 
+#[derive(Debug, PartialEq, Eq)]
 enum Line {
     Nothing,
     AcquisitionPort(i64),
@@ -106,7 +109,7 @@ fn parse_acquisition_port<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a
 fn parse_ipset<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, IpSet, E> {
     alt((
         map(tag("all"), |_| IpSet::All),
-        map_res(take_while1(|c: char| c.is_whitespace()), |p: &str| p.parse::<IpNet>().map(|c| IpSet::Cidr(c))),
+        map_res(take_while1(|c: char| !c.is_whitespace()), |p: &str| p.parse::<IpNet>().map(|c| IpSet::Cidr(c))),
     ))(i)
 }
 
@@ -117,9 +120,41 @@ fn parse_allow<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, Line,
     )(i)
 }
 
+fn parse_address<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, IpAddr, E> {
+    map_res(take_while1(|c: char| !c.is_whitespace()), |p: &str| p.parse::<IpAddr>())(i)
+}
+
+fn parse_bindacqaddress<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, Line, E> {
+    preceded(
+        tag("bindacqaddress"),
+        preceded(space1, map(parse_address, |p| Line::BindAcqAddress(p))),
+    )(i)
+}
+
 fn parse_line<'a, E: ParseError<&'a str>>(i: &'a str) -> IResult<&'a str, Line, E> {
     alt((
       parse_acquisition_port,
       parse_allow,
+      parse_bindacqaddress,
     ))(i)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::net::Ipv4Addr;
+    #[test]
+    fn parse_test() {
+        let cases = vec![
+            ("allow all", Line::Allow(IpSet::All)),
+            ("allow 127.0.0.1/32", Line::Allow(IpSet::Cidr("127.0.0.1/32".parse().unwrap()))),
+            ("bindacqaddress 1.2.3.4", Line::BindAcqAddress(IpAddr::V4(Ipv4Addr::new(1, 2, 3, 4)))),
+        ];
+
+        for case in cases {
+            let res: Result<_, nom::Err<VerboseError<&str>>> = parse_line(case.0);
+            assert_eq!(res, Ok(("", case.1)));
+        }
+
+    }
 }
